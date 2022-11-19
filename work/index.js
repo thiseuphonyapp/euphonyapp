3c2826c142673255a33efcab0c8179026a8bcc9b
leonardo <84354110+lrivas3@users.noreply.github.com>
2022-11-19 02:03:27 -0600

    Working on serving HTML

--- work/index.js
+++ work/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// CHECK CONNECTION TO DB
 const MongoClient = require('mongodb').MongoClient
 
 // Connection URL
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
-app.get('/', (req, res) => {
+app.get('/check', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
       res.status(500).send('💥 BOOM!! 💥: ' + err);
@@ -20,3 +22,8 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
 
+// SERVE HTML
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index-one.html");
+})
