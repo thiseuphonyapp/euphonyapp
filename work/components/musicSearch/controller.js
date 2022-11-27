4af48845b41e7482145811ed5eaad419e3f4095d
leonardo <84354110+lrivas3@users.noreply.github.com>
2022-11-27 11:08:47 -0600

    Configuring some routes on /components/musicSearch/network
    post => There is a problem receiving the body of the request
      - Cannot get body, as a consequence, req.body.user and
        req.body.message are undefined.
      - The controller for the function called in the post request does work

--- work/components/musicSearch/controller.js
+++ work/components/musicSearch/controller.js
@@ -1,17 +1,27 @@
 // donde va la logica de negocio
-const store=require('./store');
+const store= require('./store');
 
 // Se quiere buscar artistas por genero
 
 function getSongsFromArtist(artista){
-    console.log(artista);
-    const fullSearch = {
+    const fullSearch = { //Tendrá toda la información => por si necesito agregar más propiedades
         artista: artista,
-        date: new Date()
-
     }
+    console.log(fullSearch);
+};
+
+function addComment(user, comment){
+
+    const fullComment = { //Tendrá toda la información => por si necesito agregar más propiedades
+        user: user,
+        comment: comment,
+        date: new Date(),
+    }
+    
+    console.log(fullComment);
 };
 
 module.exports = { //exportamos todas las funciones que querramos
     getSongsFromArtist,
+    addComment,
 };
--- work/components/musicSearch/network.js
+++ work/components/musicSearch/network.js
@@ -1,45 +1,44 @@
 require("dotenv").config(); // a zero-dependency module that loads environment variables from a .env file into process.env
 const express = require('express');
-const app = express(); 
 const bodyParser = require('body-parser');
 const response = require('../../network/response'); 
 const controller = require('./controller');
-const { Router } = require('express');
-
 const router = express.Router();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(router);
 
-/*
-// CHECK CONNECTION TO DB
-const MongoClient = require('mongodb').MongoClient
-
-// Connection URL
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
-
-router.get('/check', (req, res) => {
-  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
-    if (err) {
-      res.status(500).send('💥 BOOM!! 💥: ' + err);
-    } else {
-      res.send('Me conecté a la DB! 😎');
-      db.close();
-    }
-  });
-});*/
-
+// REMEMBER TO ADD DATABASE CONNECTION
 
 
 router.get('/', function (req, res) { 
 
-  controller.getSongsFromArtist(req.body.artist);
 
   if (req.query.error == "ok"){
     response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación')
   } else {
     response.success(req, res, 'Busqueda correcta');
   }
+});
+
+router.post('/', function (req, res) {
+  console.log(req.body)
+  controller.addComment(req.body.user, req.body.message);
+  if (req.query.error == "ok"){
+    response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación')
+  } else {
+    response.success(req, res, 'Busqueda correcta');
+  }
+  
+ /* const {user, comment} = req.body;
+  messagge(user,comment)
+  .then((info)=>{
+      response.success(req,res,info,201)
+  })
+  .catch((e)=>{
+      response.error(req,res,'Informacion Invalida',400,e)
+  })*/
 });
 
 module.exports = router;
